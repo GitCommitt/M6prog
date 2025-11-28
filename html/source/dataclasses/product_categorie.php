9f9f13dce6b924e74c2af23bd03f152c361289bd
Daan Pronk <daanpronk570@gmail.com>
2025-11-28 12:29:43 +0100

    04-02 - Bijna af

--- html/source/dataclasses/product_categorie.php
+++ html/source/dataclasses/product_categorie.php
@@ -1,14 +1,23 @@
 <?php
-class product_categorie 
-{   
-        public int $idproduct_categorie;
-        public string $category_name;
+
+class product_categorie
+{
+    public int $idproduct_categorie;
+    public string $category_name;
+
     public function __construct(
         int $idproduct_categorie,
-        string $category_name,
-    ) 
-    {
+        string $category_name
+    ) {
         $this->idproduct_categorie = $idproduct_categorie;
         $this->category_name = $category_name;
+    }
+
+    public static function FromResultRow($row)
+    {
+        return new product_categorie(
+            $row['idproduct_categorie'],
+            $row['category_name']
+        );
     }
 }
