a0f9cabca39704ff2c24a6bd0aac20a0ba053e4e
Daan Pronk <daanpronk570@gmail.com>
2025-12-09 15:29:01 +0100

    04-02-01

--- Week 3 api/html/source/controllers/bericht.php
+++ Week 3 api/html/source/controllers/bericht.php
@@ -1,2 +1,17 @@
 <?php
-echo "test";
+
+function handleGet($request_url, $connection) {
+    if (sizeof($request_url) < 3) {
+        return bericht::GetAllBericht($connection);
+    } else {
+        return bericht::GetBerichtById($connection, $request_url[2]);
+    }
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+
+    $result = handleGet($request_url, $connection);
+
+    header('Content-Type: application/json; charset=utf-8');
+    print_r(json_encode($result->fetch_all(MYSQLI_ASSOC)));
+}
--- Week 3 api/html/source/dataclasses/bericht.php
+++ Week 3 api/html/source/dataclasses/bericht.php
@@ -16,13 +16,24 @@ class bericht
         $this->user_iduser = $user_iduser;
     }
 
-    public function GetAllBericht(){
-        
+    public static function GetAllBericht($connection){
+        $query = "SELECT * FROM bericht";
+        $stmt = $connection->prepare($query);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result;
     }
 
 
-    public function GetBerichtById(){
+    public static function GetBerichtById($connection, $idbericht){
+        $query = "SELECT * FROM bericht WHERE idbericht = ?";
+        $stmt = $connection->prepare($query);
+        $stmt->bind_param("s", $idbericht);
+        $stmt->execute();
+        $result = $stmt->get_result();
         
+        return $result;
     }
 }
 
